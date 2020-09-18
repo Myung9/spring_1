f94e1d9128158cf28c4f2900a165c50a132997ce
m9 <ymgu0709@gmail.com>
2020-09-18 09:36:07 +0900

    Lombok 생성자 어노테이션

--- src/main/java/com/myung9/spring/project1/Mycontact/domain/Person.java
+++ src/main/java/com/myung9/spring/project1/Mycontact/domain/Person.java
@@ -1,6 +1,7 @@
 package com.myung9.spring.project1.Mycontact.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Setter
 @Entity
 @ToString
+@NoArgsConstructor
 public class Person {
     @Id
     @GeneratedValue //자동으로 생성
--- src/test/java/com/myung9/spring/project1/Mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/myung9/spring/project1/Mycontact/repository/PersonRepositoryTest.java
@@ -5,12 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PersonRepositoryTest {
