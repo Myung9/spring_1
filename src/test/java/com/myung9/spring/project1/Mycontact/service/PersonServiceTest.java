e1129d781807eb4a83ad7975d5deb706eded52eb
m9 <ymgu0709@gmail.com>
2021-02-22 13:56:14 +0900

    service test
    서비스 로직의 테스트는 일반적으로 스프링 컨텍스트가 필요없는 것이 대부분
    스프링 컨텍스트 로딩이 없다면 더 테스트가 더 빠르고 디테일하게 처리
    일반적으로 서비스레이어의 테스트가 가장 탄탄하게 만들어져 있다.
    실제로 버그가 발생하는 영역도 서비스 레이어가 많다
    이번 테스트에는 @SpringBootTest가 아니고 @Mockito를 사용
    
    서비스 테스트를 위해 mockito 의존성을 등록하고 사용

--- src/test/java/com/myung9/spring/project1/Mycontact/service/PersonServiceTest.java
+++ src/test/java/com/myung9/spring/project1/Mycontact/service/PersonServiceTest.java
@@ -2,32 +2,31 @@ package com.myung9.spring.project1.Mycontact.service;
 
 import com.myung9.spring.project1.Mycontact.domain.Person;
 import com.myung9.spring.project1.Mycontact.repository.PersonRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /*서비스 테스트경우 보통 mocktest를 많이 사용*/
-@SpringBootTest
+@ExtendWith(MockitoExtension.class) // mockito 기능을 사용해서 테스트를 진
 class PersonServiceTest {
-    @Autowired
+    @InjectMocks // 테스트의 주체 // 테스트의 대상이 되는 곳에 달아주는 어노테이션
     private PersonService personService;
-    @Autowired
+    @Mock//PersonService에 의존성을 가지고 있는 PersonRepository
     private PersonRepository personRepository;
-
-
-
-    @Test
-    void getPerson(){
-        Person person = personService.getPerson(3L);
-        assertThat(person.getName()).isEqualTo("dennis");
-    }
 
     @Test
     void getPeopleByName(){
+        when(personRepository.findByName("martin"))
+           .thenReturn(Lists.newArrayList(new Person("martin")));
+
         List<Person> result = personService.getPeopleByName("martin");
 
         assertThat(result.size()).isEqualTo(1);
